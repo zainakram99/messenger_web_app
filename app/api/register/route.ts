db0cd8c9f78df49f746a708908e6357091e5dbcc
ZAIN AKRAM <zainakram3376@gmail.com>
2024-03-17 02:56:31 +0500

    half project completed

---
+++ app/api/register/route.ts
@@ -0,0 +1,31 @@
+import bcrypt from "bcrypt";
+
+import prisma from "@/app/libs/prismadb";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { name, email, password } = body;
+    console.log(body, body);
+
+    if (!name || !email || !password) {
+      return new NextResponse("Missing data", { status: 400 });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error: any) {
+    console.log(error, "REGISTRATION_ERROR");
+    return new NextResponse("Missing info", { status: 400 });
+  }
+}
